4496ef30953c8109f71fa392ef16a0c1f88ffc3e
JonSnow47 <745539141@qq.com>
2018-04-02 14:52:52 +0800

    complete Bisection Method

--- search/bisection/bisection.go
+++ search/bisection/bisection.go
@@ -1,17 +1,41 @@
 package main
 
-func Bisection(list []int64, item int64) (int64, error) {
+import (
+	"fmt"
+)
+
+const NotExist = -1
+
+func bisection(list []int64, item int64) (int64, bool) {
 	var low, mid, high int64
-	low, high = 0, int64(len(list) - 1)
-	mid = int64(low + high) / 2
+	low, high = 0, int64(len(list)-1)
 
 	for low <= high {
-		if item == list[mid] {
-			return mid, nil
-		} else if item >= list[mid] {
-			high = mid + 1
+		// the best way to get mid:	mid = low + (high - low) /2
+		// 避免值溢出
+		mid = int64((low + high) / 2)
+		if list[mid] == item {
+			return mid, true
+		} else if list[mid] >= item {
+			high = mid - 1
 		} else {
-			low = mid - 1
+			low = mid + 1
 		}
+	}
+	return NotExist, false
+}
+
+func main() {
+	list := []int64{1, 3, 5, 7, 9}
+	var item int64
+
+	fmt.Print("Please enter your number: ")
+	fmt.Scanln(&item)
+
+	place, flag := bisection(list, item)
+	if !flag {
+		fmt.Println("Not exist.")
+	} else {
+		fmt.Printf("%d in %d place.", item, place)
 	}
 }
