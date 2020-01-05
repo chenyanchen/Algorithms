b27a8f55416b8fac255488d856cf3872379f699b
JonSnow47 <745539141@qq.com>
2020-01-05 21:17:25 +0800

    merge sort

---
+++ sort/merge_sort/merge sort.go
@@ -0,0 +1,31 @@
+package merge_sort
+
+func MergeSort(arr []int) []int {
+	l := len(arr)
+	if l <= 1 {
+		return arr
+	}
+
+	mid := l / 2
+	left := MergeSort(arr[:mid])
+	right := MergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) (result []int) {
+	l, r := 0, 0
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			result = append(result, left[l])
+			l++
+		} else {
+			result = append(result, right[r])
+			r++
+		}
+	}
+
+	result = append(result, left[l:]...)
+	result = append(result, right[r:]...)
+	return
+}
---
+++ sort/merge_sort/merge_sort_test.go
@@ -0,0 +1,29 @@
+package merge_sort
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func TestMergeSort(t *testing.T) {
+	num := rand.Intn(1000)
+	arr := make([]int, num)
+	for i := range arr {
+		arr[i] = rand.Int()
+	}
+
+	result := MergeSort(arr)
+	sort.Ints(arr)
+
+	for i := range arr {
+		if arr[i] != result[i] {
+			t.Logf("expect: %d got: %d", arr[i], result[i])
+		}
+	}
+}
